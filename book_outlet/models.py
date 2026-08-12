0d4491bb4a2ad51e1959ddcb8c22ae662f34426d
Krishna Sharma <krishna29.arya@gmail.com>
2026-08-12 20:11:17 +0530

    Learn handling the admin area

--- book_outlet/models.py
+++ book_outlet/models.py
@@ -11,7 +11,9 @@ class Book(models.Model):
     )
     author = models.CharField(null=True, max_length=100)
     is_bestselling = models.BooleanField(default=False)
-    slug = models.SlugField(default="", null=False, db_index=True)
+    slug = models.SlugField(
+        default="", blank=True, null=False, db_index=True
+    )
 
     def get_absolute_url(self):
         return reverse("book-details", args=[self.slug])
